42df72e73364314e8b343b7f2e1911e5989fe16e
Xiye <84062739+Xiyeh@users.noreply.github.com>
2022-06-18 20:27:56 -0700

    Css change

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 import "./header.css";
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
+    return (
+        <header>
+            <div className="container header-container">
+                <h1>Vincent Huang</h1>
+                <h5 className="gray-text">3rd Year Computer Science and Data Science Student at UC Davis</h5>
+            </div>
+        </header>
+    )
 }
 
 export default Header
