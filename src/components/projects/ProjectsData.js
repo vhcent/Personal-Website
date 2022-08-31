6d72e0d80d19ec82a8aaa56791e14f4774cd52c8
Vincent Huang <vhuang2020@gmail.com>
2022-08-30 20:40:52 -0700

    Added descriptions to projects

--- src/components/projects/ProjectsData.js
+++ src/components/projects/ProjectsData.js
@@ -8,20 +8,23 @@ export const ProjectsData = [
         icon: personalWebsite,
         link: "",
         github: "https://github.com/vhcent/Personal-Website",
-        description: "description",
+        description:
+            "A personal portfolio made from HTML, CSS, JavaScript, React, and EmailJS which displays my information, skills, and experience.",
     },
     {
         title: "Pathfinding Algorithm Visualizer",
         icon: pathfinding,
         link: "https://pathfindingalgovisual.herokuapp.com/",
         github: "https://github.com/vhcent/Pathfinding-Algorithm-Visualizer",
-        description: "description",
+        description:
+            "An algorithm visualizer build from HTML, CSS, JavaScript, and React that showcases various graph algorithms such as Dijkstra's algorithm, Breadth First Search, and Depth First Search. Includes options for adjusting terrain, adding weights, changing speeds, among many.",
     },
     {
         title: "Sorting Algorithm Visualizer",
         icon: sorting,
         link: "https://sortingalgovisual.herokuapp.com/",
         github: "https://github.com/AroopBiswal/Sorting-Algorithm-Visualizer",
-        description: "description",
+        description:
+            "An algorithm visualizer which depicts classic sorting algorithms made from HTML, CSS, JavaScript, and React. Displays classic algorithms such as merge sort, quick sort, heap sort, etc. Provides options for visualization speed and element size.",
     },
 ];
